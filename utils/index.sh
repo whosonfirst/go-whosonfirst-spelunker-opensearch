8b481dc03a157403fa23f49f22b1e1661c5cd891
thisisaaronland <thisisaaronland@localhost>
2024-04-02 09:13:12 -0700

    improved docs for utils/index.sh

--- utils/index.sh
+++ utils/index.sh
@@ -5,12 +5,14 @@
 # purpose tool so you may need to adjust details to suit your specific
 # needs.
 
+# https://github.com/whosonfirst/go-whosonfirst-github
 LIST_REPOS=/usr/local/whosonfirst/go-whosonfirst-github/bin/wof-list-repos
+
+# https://github.com/whosonfirst/go-whosonfirst-opensearch
+OPENSEARCH_INDEX=/usr/local/whosonfirst/go-whosonfirst-opensearch/bin/wof-opensearch-index
 
 # Maybe make this a CLI option...
 LIST_REPOS_PREFIX=whosonfirst-data-admin-a
-
-OPENSEARCH_INDEX=/usr/local/whosonfirst/go-whosonfirst-opensearch/bin/wof-opensearch-index
 
 # This is for debugging. Do not change this at your own risk.
 # (That means you should change this.)
@@ -18,6 +20,7 @@ OPENSEARCH_PASSWD=dkjfhsjdkfkjdjhksfhskd98475kjHkzjxckj
 
 OPENSEARCH_WRITER_URI=constant://?val=opensearch2%3A%2F%2Flocalhost%3A9200%2Fspelunker%3Frequire-tls%3Dtrue%26insecure%3Dtrue%26debug%3Dfalse%26username%3Dadmin%26password%3D${OPENSEARCH_PASSWD}
 
+# Pull straight from GitHub, write to /tmp, remove after indexing
 ITERATOR_URI=git:///tmp
 
 REPOS=`${LIST_REPOS} -prefix ${LIST_REPOS_PREFIX} -exclude whosonfirst-data-admin-alt`
